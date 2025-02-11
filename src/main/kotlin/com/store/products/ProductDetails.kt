ad748d1693daf449cfa78a05d7cc27330cf7464b
Prasad Sawool <thc.prasads@gmail.com>
2025-02-11 23:45:58 +0530

    refactor(products): modularize products service

---
+++ src/main/kotlin/com/store/products/ProductDetails.kt
@@ -0,0 +1,19 @@
+package com.store.products
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.annotation.Nullable
+import jakarta.validation.constraints.*
+
+data class ProductDetails(
+    @JsonProperty("id") val id: Int,
+    @JsonProperty("name") @field:Pattern(
+        regexp = "(?!true|false|null)[a-zA-Z]+", message = "should be a string"
+    ) @field:NotEmpty(message = "Name cannot be empty") val name: String,
+    @JsonProperty("type") val type: ProductType,
+    @JsonProperty("inventory") @field:NotNull @field:Min(1) @field:Max(9999) val inventory: Int,
+    @JsonProperty("cost") @field:Nullable val cost: Double?
+)
+
+enum class ProductType {
+    book, food, gadget, other
+}
